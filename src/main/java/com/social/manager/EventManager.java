582db77726127d5e14241ae2857b48af8a7f5aab
Irene Guijarro Martinez <iguijarm@everis.com>
2018-03-21 17:11:49 +0100

    AuthController, implementacion de jwt, generación y verificación+

--- src/main/java/com/social/manager/EventManager.java
+++ src/main/java/com/social/manager/EventManager.java
@@ -1,8 +1,11 @@
 package com.social.manager;
 
+import java.util.List;
+
 import com.social.entity.Event;
 import com.social.entity.Person;
 
 public interface EventManager extends Manager<Event> {
 	public void addPerson(Event event, Person person);
+	public List<Event> getByCreatorId(Long id);
 }
