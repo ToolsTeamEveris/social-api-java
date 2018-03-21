88f662a12978469c11bc4e49f6218df2a1a4a277
USERSAD\jamorest <joseamores007@hotmail.com>
2018-03-21 17:53:21 +0100

    Hecho lo del update de personas

--- src/main/java/com/social/controller/PersonController.java
+++ src/main/java/com/social/controller/PersonController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -52,6 +54,12 @@ public class PersonController  {
 	@ResponseBody
 	public List<Person> GetFriends(@RequestBody Person person) {
 		return person.getFriends();
+	}
+	
+	@PutMapping(value="/person/{id}/update")
+	@ResponseBody
+	public void UpdatePerson(@RequestBody Person person) {
+		manager.update(person);
 	}
 	
 	// TODO relate  -> coger id del token cuando este
--- src/main/java/com/social/manager/PersonManager.java
+++ src/main/java/com/social/manager/PersonManager.java
@@ -4,4 +4,6 @@ import com.social.entity.Person;
 
 public interface PersonManager extends Manager<Person> {
 	Person relatePerson(Long id);
+	void updatePerson(Long id, Person person);
+	
 }
