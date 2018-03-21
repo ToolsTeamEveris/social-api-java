3cdab522f46d2388e2a007a38aeb0c97c925a8f3
Irene Guijarro Martinez <iguijarm@everis.com>
2018-03-21 11:51:17 +0100

    conflicto Person.java resuelto

--- src/main/java/com/social/entity/Person.java
+++ src/main/java/com/social/entity/Person.java
@@ -6,13 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-<<<<<<< HEAD
 
 import lombok.Data;
-
-=======
-import javax.persistence.SequenceGenerator;
->>>>>>> 4f2f27542d5fa75bb55ce7716482da987ee96d25
 
 @Entity
 @Data
@@ -24,42 +19,5 @@ public class Person {
 	private String name;
 	private String surname;
 	private List<Person> friends;
-	
-<<<<<<< HEAD
-//	public String getName() {
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	public String getSurname() {
-//		return surname;
-//	}
-//	public void setSurname(String surname) {
-//		this.surname = surname;
-//	}
-=======
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getSurname() {
-		return surname;
-	}
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-	public List<Person> getFriends() {
-		return friends;
-	}
-	public void setFriends(List<Person> friends) {
-		this.friends = friends;
-	}
-	public Long getId() {
-		return id;
-	}
->>>>>>> 4f2f27542d5fa75bb55ce7716482da987ee96d25
 	
 }
