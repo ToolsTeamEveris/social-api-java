58073ddd3ecc92c203753104f4ddce5a20e56057
Ivan Galan Pastor <igalanpa@everis.com>
2018-03-22 15:16:35 +0100

    implement group controller

--- src/main/java/com/social/controller/GroupController.java
+++ src/main/java/com/social/controller/GroupController.java
@@ -11,27 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.social.entity.Group;
 import com.social.entity.Person;
-import com.social.manager.GroupManagerImpl;
-import com.social.manager.PersonManagerImpl;
+import com.social.manager.GroupManager;
+import com.social.manager.PersonManager;
+
+import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class GroupController {
 
-	private GroupManagerImpl manager;
-        private PersonManagerImpl personManager;
+	private final GroupManager manager;
+        private final PersonManager personManager;
 	
 	@Autowired
-	public GroupController(final GroupManagerImpl groupManager) {
+	public GroupController(final GroupManager groupManager, final PersonManager personManager) {
             this.manager = groupManager;
+            this.personManager = personManager;
 	}
 	
 	@RequestMapping(value = "/group", method = RequestMethod.GET)
 	@ResponseBody
-	public List<Group> getAll(){
-            List<Group> list = new ArrayList<Group>();
-            this.manager.findAll().forEach( g -> list.add(g));
-            return list;
+	public Iterable<Group> getAll(){
+            return this.manager.findAll();
 	}
 	
 	@RequestMapping(value = "/group/{id}", method = RequestMethod.GET)
@@ -43,30 +44,43 @@ public class GroupController {
 	@RequestMapping(value = "/group", method = RequestMethod.POST)
 	@ResponseBody
 	public Group create(@RequestBody Group group) {
+            
+            //TODO get id from token
+            Person user = this.personManager.findById(1000l);
+            
+            group.setCreator(user);
+            group.setMembers(new ArrayList<Person>());
+            
             this.manager.save(group);
+            
             return group;
         }
 	
-	@RequestMapping(value = "/group/person/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/group/person/{personId}", method = RequestMethod.GET)
 	@ResponseBody
-	public Group getByPersonId(@PathVariable Long personId) {
+	public List<Group> getByPersonId(@PathVariable Long personId) {
             List<Group> groups = new ArrayList<>();
             this.manager.findAll().forEach( g -> groups.add(g));
-            //TODO implement creator at group
-            
-            return null;
+
+            //TODO make a custom query
+            return groups.stream().filter( g -> g.getCreator().getId().equals(personId)).collect(Collectors.toList());
         }
 	
 	@RequestMapping(value = "/group/{id}/relate", method = RequestMethod.POST)
 	@ResponseBody
-	public void getAll(@PathVariable Long id , List<Long> list){
+	public void getAll(@PathVariable Long id , @RequestBody ArrayList<Long> list){
             Group group = this.manager.findById(id);
+            List<Person> persons = new ArrayList<>();
             
-            list.stream().map( userId -> {
-                Person person = this.personManager.findById(userId);
-                group.addPerson(person);
-                return person;
-            });
+            System.out.println(list);
+
+            list.stream()
+                    .forEach(userId -> persons.add(this.personManager.findById(userId)));
+            
+            group.addPerson(persons);
+            
+            System.out.println(persons);
+            System.out.println(group);
             
             this.manager.save(group);
 	}
--- src/main/java/com/social/entity/Group.java
+++ src/main/java/com/social/entity/Group.java
@@ -6,23 +6,38 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name="grupo")
 public class Group {
-	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
-	@OneToMany(mappedBy = "id")
-	private List<Person> friends;
+        @ManyToOne
+	@JoinColumn(name="creator")
+	private Person creator;
+	@OneToMany
+	@JoinTable(
+            name="members",
+            joinColumns=@JoinColumn(name="Person", referencedColumnName="id"))
+	private List<Person> members;
 	private String name;
-	private byte[] picture;
+	//private byte[] picture;
 	
         public void addPerson(Person person) {
-            this.friends.add(person);
+            this.members.add(person);
         }
+        
+        public void addPerson(List<Person> persons) {
+            persons.stream().forEach(p -> this.members.add(p));
+        }
+        
 }
