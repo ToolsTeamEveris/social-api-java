b98fcf1bf1b63049b745bab087e7949ba9f49e1c
USERSAD\jamorest <joseamores007@hotmail.com>
2018-03-27 15:17:50 +0200

    corregido error en sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-insert into Person (id, name, surname, username, password) values (1000, 'Ivan', 'Galan', 'ivan', 'ivan';
-insert into Person (id, name, surname, username, password) values (1002, 'Irene', 'Guijarro', 'irene', 'irene',);
+insert into Person (id, name, surname, username, password) values (1000, 'Ivan', 'Galan', 'ivan', 'ivan');
+insert into Person (id, name, surname, username, password) values (1002, 'Irene', 'Guijarro', 'irene', 'irene');
 insert into Person (id, name, surname, username, password) values (1003, 'Jose', 'Amores', 'jose', 'jose');
 
 insert into Event (id, ending_date, name, starting_date, type, creator) values (1000, '2018-03-21', 'Beber agua','2018-03-22', 0, 1002);
 
 
-insert into Post (id, creator, text, creation_date, lat, lng ) values (1000, 1002, 'aaaa', '2018-03-21', 38.4, -1));
+insert into Post (id, creator, text, creation_date, lat, lng ) values (1000, 1002, 'aaaa', '2018-03-21', 38.4, -1);
 insert into Post (id, creator, text, creation_date, lat, lng ) values (1001, 1002, 'eeee', '2018-03-21', 38.4, -2);
 insert into Post (id, creator, text, creation_date, lat, lng ) values (1002, 1003, 'rrrr', '2018-03-21', 38.4, -0.5);
 
