3a1d91334e22f480303a351f7938c574707c289e
USERSAD\dgimenev <dionijv@gmail.com>
2018-03-29 11:45:23 +0200

    Implementar las preferencias de usuario en las person

--- src/main/java/com/social/controller/PersonController.java
+++ src/main/java/com/social/controller/PersonController.java
@@ -64,19 +64,10 @@ public class PersonController  {
 		if (person.getName() != null) p.setName(person.getName());
 		if (person.getSurname() != null) p.setSurname(person.getSurname());
 		//if (person.getPicture() != null) p.setPicture(person.getPicture());
-<<<<<<< HEAD
-		// Updateamos		
-		manager.update(p);
-		// Updateamos	
-		//Hay que hacer update de p no de person, no?
-		manager.update(person);
-=======
-		
 
 		// Updateamos		
 		manager.update(p);
 
->>>>>>> solucion-primarykey-friend
 	}
 	/*
 	// TODO relate  -> coger id del token cuando este
--- src/main/java/com/social/entity/Person.java
+++ src/main/java/com/social/entity/Person.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 
 import javax.persistence.Entity;
@@ -17,8 +19,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import lombok.Data;
+
+import org.hibernate.engine.internal.Cascade;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -35,6 +40,9 @@ public class Person implements UserDetails {
     private String password;
 	private String name;
 	private String surname;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="userPreferences")
+	private UserPreferences userPreferences;
 
 
 
